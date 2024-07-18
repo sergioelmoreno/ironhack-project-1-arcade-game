5f395d2b48b8085b74897fdca6dd0e3ae18c4510
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-07-18 13:00:36 +0200

    Añadimos imagenes de enemigos

--- js/game.js
+++ js/game.js
@@ -1,142 +1,145 @@
 
 
 const Game = {
-    // Propiedades
-    gameDimensions: {
-        h: window.innerHeight,
-        w: window.innerWidth,
-    },
-    frameCounter: 0,
-    player1: undefined,
-    background1: undefined,
-    roadside1: undefined,
-    enemies: [],
-    playerLives: undefined,
-    keys: {
-        left: "ArrowLeft",
-        right: "ArrowRight",
-        up: "ArrowUp",
-        down: "ArrowDown"
-    },
-    isKeyPressed: {
-        isPressed: false,
-        keyCode: ""
-    },
-    isGameOver: false,
-    gameEngine: undefined,
+  // Propiedades
+  gameDimensions: {
+    h: window.innerHeight,
+    w: window.innerWidth,
+  },
+  frameCounter: 0,
+  player1: undefined,
+  background1: undefined,
+  roadside1: undefined,
+  enemies: [],
+  playerLives: undefined,
+  keys: {
+    left: "ArrowLeft",
+    right: "ArrowRight",
+    up: "ArrowUp",
+    down: "ArrowDown"
+  },
+  isKeyPressed: {
+    isPressed: false,
+    keyCode: ""
+  },
+  isGameOver: false,
+  gameEngine: undefined,
 
-    // Metodos
-    init() {
-        this.setSize()
-        this.counterFrames()
-        this.setEventsListener()
-        this.background1 = new Background(this.gameDimensions)
-        this.roadside1 = new Roadside(this.gameDimensions, this.background1.positionBackground1.left, this.background1.size.width)
-        this.player1 = new Player(this.gameDimensions)
-        this.playerLives = new Lives(this.gameDimensions)
-        this.calculateEmenyLeft()
-    },
+  // Metodos
+  init() {
+    this.setSize()
+    this.counterFrames()
+    this.setEventsListener()
+    this.background1 = new Background(this.gameDimensions)
+    this.roadside1 = new Roadside(this.gameDimensions, this.background1.positionBackground1.left, this.background1.size.width)
+    this.player1 = new Player(this.gameDimensions)
+    this.playerLives = new Lives(this.gameDimensions)
+    this.calculateEmenyLeft()
+  },
 
-    setSize() {
-        document.querySelector("#game-screen").style.width = this.gameDimensions.w
-        document.querySelector("#game-screen").style.height = this.gameDimensions.h
-        document.querySelector("#game-screen").style.background = "blue"
-    },
-    calculateEmenyLeft() {
-        const left = this.background1.positionBackground1.left
-        const width = this.background1.size.width
-        let mult = Math.random()
-        return left + (width * mult)
-    },
-    counterFrames() {
-        if (!this.isGameOver) {
-            this.gameEngine = setInterval(() => {
-                if (this.frameCounter === 5000) {
-                    this.frameCounter = 0
-                } else {
-                    this.frameCounter++
-                }
-                if (this.frameCounter % 100 === 0) {
-                    console.log(this.frameCounter)
-                    this.enemy = new Enemy(this.gameDimensions, this.calculateEmenyLeft())
-                    this.enemies.push(this.enemy)
-                    console.log(this.enemies)
-                }
-
-
-                /*if (this.frameCounter === 50) {
-                    this.enemy1 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.17))
-                    this.enemies.push(this.enemy1)
-                }
-                else if (this.frameCounter === 100) {
-                    this.enemy2 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.12))
-                    this.enemies.push(this.enemy2)
-                }
-                else if (this.frameCounter === 150) {
-                    this.enemy3 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.55))
-                    this.enemies.push(this.enemy3)
-                }*/
-
-                this.updateObjects()
-
-
-            }, 20)
+  setSize() {
+    document.querySelector("#game-screen").style.width = this.gameDimensions.w
+    document.querySelector("#game-screen").style.height = this.gameDimensions.h
+    document.querySelector("#game-screen").style.background = "blue"
+  },
+  calculateEmenyLeft() {
+    const left = this.background1.positionBackground1.left
+    const width = this.background1.size.width
+    let mult = Math.random()
+    return left + (width * mult)
+  },
+  counterFrames() {
+    if (!this.isGameOver) {
+      this.gameEngine = setInterval(() => {
+        if (this.frameCounter === 5000) {
+          this.frameCounter = 0
         } else {
-            clearInterval(this.gameEngine)
+          this.frameCounter++
+        }
+        if (this.frameCounter % 100 === 0) {
+          console.log(this.frameCounter)
+          this.enemy = new Enemy(this.gameDimensions, this.calculateEmenyLeft())
+          this.enemies.push(this.enemy)
+          console.log(this.enemies)
         }
 
-        console.log(this.enemies)
 
-    },
-
-    setEventsListener() {
-        // TO-DO: Manage simultaneous “keyPressed” Events 
-        // check this out: https://medium.com/@joshbwasserman/managing-simultaneous-keypressed-events-in-javascript-78da1b3b14de
-        const handlePressed = (event) => {
-            if (Object.values(this.keys).indexOf(event.code) > -1) {
-                this.isKeyPressed.isPressed = true
-                this.isKeyPressed.keyCode = event.code
-            }
+        /*if (this.frameCounter === 50) {
+            this.enemy1 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.17))
+            this.enemies.push(this.enemy1)
         }
-        const handleReleased = (event) => {
-            if (event.type === "keyup") {
-                this.isKeyPressed.isPressed = false
-                this.isKeyPressed.keyCode = ""
-            }
+        else if (this.frameCounter === 100) {
+            this.enemy2 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.12))
+            this.enemies.push(this.enemy2)
         }
-        document.addEventListener("keydown", event => handlePressed(event))
-        document.addEventListener("keyup", event => handleReleased(event))
-    },
+        else if (this.frameCounter === 150) {
+            this.enemy3 = new Enemy(this.gameDimensions, this.calculateEmenyLeft(.55))
+            this.enemies.push(this.enemy3)
+        }*/
 
-    updateObjects(keyCode) {
-        this.roadside1.move()
-        this.background1.move()
+        this.updateObjects()
 
-        this.enemies.forEach((enemy, index) => {
-            enemy.move()
 
-            if (enemy.position.top >= this.gameDimensions.h) {
-                this.enemies.splice(enemy[index], 1)
-                enemy.enemyElement.remove()
-
-            }
-
-        })
-
-        if (!this.isKeyPressed) {
-            this.player1.move(this.isKeyPressed.isPressed)
-        } else {
-            this.player1.move(this.isKeyPressed.keyCode)
-        }
-        if (this.playerLives.lives.current === 0) {
-            this.gameOver()
-        }
-
-    },
-
-    gameOver() {
-        this.isGameOver = true
-        alert("GAME OVER")
+      }, 20)
+    } else {
+      clearInterval(this.gameEngine)
     }
+
+    console.log(this.enemies)
+
+  },
+
+  setEventsListener() {
+    // TO-DO: Manage simultaneous “keyPressed” Events 
+    // check this out: https://medium.com/@joshbwasserman/managing-simultaneous-keypressed-events-in-javascript-78da1b3b14de
+    const handlePressed = (event) => {
+      if (Object.values(this.keys).indexOf(event.code) > -1) {
+        this.isKeyPressed.isPressed = true
+        this.isKeyPressed.keyCode = event.code
+      }
+    }
+    const handleReleased = (event) => {
+      if (event.type === "keyup") {
+        this.isKeyPressed.isPressed = false
+        this.isKeyPressed.keyCode = ""
+      }
+    }
+    document.addEventListener("keydown", event => handlePressed(event))
+    document.addEventListener("keyup", event => handleReleased(event))
+  },
+
+  updateObjects(keyCode) {
+    this.roadside1.move()
+    this.background1.move()
+
+    this.enemies.forEach((enemy, index) => {
+      enemy.move()
+
+      if (enemy.position.top >= this.gameDimensions.h) {
+        this.enemies.splice(enemy[index], 1)
+        enemy.enemyElement.remove()
+
+      }
+
+    })
+
+    if (!this.isKeyPressed) {
+      this.player1.move(this.isKeyPressed.isPressed)
+    } else {
+      this.player1.move(this.isKeyPressed.keyCode)
+    }
+    if (this.playerLives.lives.current === 0) {
+      this.gameOver()
+    }
+    if (this.playerLives.lives.current === 0) {
+      this.gameOver()
+    }
+
+  },
+
+  gameOver() {
+    this.isGameOver = true
+    alert("GAME OVER")
+  }
 
 }
